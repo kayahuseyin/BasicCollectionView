410ce6bec2dc9ac4d6e7c3a75490f9e706cd2332
Hüseyin Kaya <88154777+kayahuseyin@users.noreply.github.com>
2023-12-13 10:52:19 +0300

    initial commit

--- BasicCollectionView/ViewController.swift
+++ BasicCollectionView/ViewController.swift
@@ -16,7 +16,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        countries = ["Türkiye", "Germany", "France", "Korea", "Sweden","Japan", "Albenia", "Montenegro"]
+        let design: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        
+        let width = self.collectionView.frame.size.width
+        
+        design.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        
+        design.itemSize = CGSize(width: (width-30)/3, height: (width-30)/3)
+        
+        design.minimumInteritemSpacing = 5
+        
+        design.minimumLineSpacing = 5
+        
+        collectionView!.collectionViewLayout = design
+        
+        countries = [
+            "Türkiye", "Germany", "France", "Korea", "Sweden", "Japan", "Albania", "Montenegro",
+            "Italy", "Spain", "Canada", "Australia", "Brazil", "China", "Egypt", "India", "Mexico",
+            "Nigeria", "Russia", "South Africa", "United Kingdom", "United States", "Argentina",
+            "Indonesia", "Pakistan", "Bangladesh", "Philippines", "Vietnam", "Ethiopia"
+        ]
         
         collectionView.delegate = self
         collectionView.dataSource = self
@@ -42,12 +61,14 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
         
         cell.countryLabel.text = countries[indexPath.item]
         
+        
         return cell
         
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) { // Item tiklandiginda calisir
         print("Secilen Ulke: \(countries[indexPath.item])")
+        
     }
     
 }
